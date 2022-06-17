6dfb6da3cd5b117c3624a4fd5afd217a9764f9a6
Matthew Liu <mattliu62@gmail.com>
2022-06-17 01:21:07 -0700

    Fixes, standalone version

--- index.js
+++ index.js
@@ -2,57 +2,69 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const extractFrames = require('ffmpeg-extract-frames');
 const clearDirectory = require('./clearDirectory');
+const prompt = require("prompt-sync")({ sigint: true });
 
-//begin input read
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-console.log('Headless Frame Splitter ver. 1.0')
-readline.question(`Enter valid YouTube Link: `, link => {
-
-//function to run ytdl download
-async function download(link) {
-    //collect video metadata
-    const requestInfo = await ytdl.getInfo(link);
-    const name = requestInfo.videoDetails.title;
+async function run() {
+    console.log('Headless Frame Splitter ver. 1.0')
+    const link = prompt("Enter valid YouTube Link: ");
     
-    console.log(`Now processing ${name}!`);
+    //create frames folder
+    const folderName = './frames';
 
-    ytdl(link, {quality: '136', filter: 'videoonly'}).pipe(fs.createWriteStream(`video.mp4`)).on('finish', function() {
-        console.log('Successfully downloaded video.');
+    try {
+    if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName);
+    }
+    } catch (err) {
+    console.error(err);
+    }
 
-        //call process function
-        process();
-    });
-};
-
-//function to run FFMPEG on frame 
-async function process() {
-    //use FFMPEG to extract frames at intervals of 3 seconds - promise returned when completed
-    await extractFrames({
-    input: 'video.mp4',
-    output: `./frames/frame-%d.png`,
-    fps: 1,
-    });
-
-    console.log('Successfully generated frames.')
-    console.log('Mission Complete!')
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
-};
-
-clearDirectory.clearDirectory();
-
-//checks to see if video URL is valid - if not, the program exits
-if (!ytdl.validateURL(link)) {
-    console.log('Invalid URL.');
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    process.exit(1);
-
-} else {
-    download(link);
+    await clearDirectory.clearDirectory();
+    
+    //function to run ytdl download
+    async function download(link) {
+        //collect video metadata
+        const requestInfo = await ytdl.getInfo(link);
+        const name = requestInfo.videoDetails.title;
+        
+        console.log(`Now processing ${name}!`);
+    
+        ytdl(link, {quality: '136', filter: 'videoonly'}).pipe(fs.createWriteStream(`video.mp4`)).on('finish', function() {
+            console.log('Successfully downloaded video.');
+    
+            //call process function
+            process();
+        });
+    };
+    
+    //function to run FFMPEG on frame 
+    async function process() {
+    
+        //use FFMPEG to extract frames at intervals of 3 seconds - promise returned when completed
+        await extractFrames({
+        input: 'video.mp4',
+        output: `./frames/frame-%d.png`,
+        fps: 1,
+        });
+    
+        console.log('Successfully generated frames.')
+        console.log('Mission Complete!')
+        setTimeout(function() {
+            console.log('Exiting...');
+        }, 3000);
+    };
+    
+    //checks to see if video URL is valid - if not, the program exits
+    if (!ytdl.validateURL(link)) {
+        console.log('Invalid URL.');
+        setTimeout(function() {
+            console.log('Exiting...');
+        }, 3000);
+        // process.exit(1);
+    
+    } else {
+        download(link);
+    }
+    
 }
-readline.close();
-});
+run();
